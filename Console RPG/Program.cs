30a7693632f9f905b6d7c52a9b72c14cb4b215d7
Derek <herrerasturmdl@mukilteo.wednet.edu>
2024-02-02 12:15:21 -0800

    yeah

---
+++ Console RPG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
